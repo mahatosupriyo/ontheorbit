af8e421ea705a43c601b5a502776175c4b807d38
Supriyo Mahato <mesupriyomahato@gmail.com>
2025-02-13 14:40:30 +0530

    added footer component

--- src/components/molecules/footer/footer.tsx
+++ src/components/molecules/footer/footer.tsx
@@ -1,6 +1,6 @@
+"use client"
 import Link from 'next/link';
 import styles from './footer.module.scss'
-import { motion } from 'framer-motion'
 import Icon from '@/components/atoms/icons';
 
 export default function Footer() {
@@ -9,59 +9,68 @@ export default function Footer() {
             <div className={styles.footerwraper}>
 
 
-                <div className={styles.linklist}>
+                <div className={styles.footercontent}>
 
-                    <div className={styles.linkcolumn}>
-                        <p className={styles.linklabel}>Features</p>
-                        <Link href="/" className={styles.linkitem}>Garage</Link>
-                        <Link href="/" className={styles.linkitem}>Space</Link>
-                        <Link href="/" className={styles.linkitem}>Voyager</Link>
+                    <div className={styles.linklist}>
+
+                        <div className={styles.linkcolumn}>
+                            <p className={styles.linklabel}>Features</p>
+                            <Link draggable="false" href="/" className={styles.linkitem}>Garage</Link>
+                            <Link draggable="false" href="/" className={styles.linkitem}>Space</Link>
+                            <Link draggable="false" href="/" className={styles.linkitem}>Voyager</Link>
+                        </div>
+
+                        <div className={styles.linkcolumn}>
+                            <p className={styles.linklabel}>Resources</p>
+
+                            <Link draggable="false" href="/" className={styles.linkitem}>Blogs</Link>
+                            <Link draggable="false" href="/" className={styles.linkitem}>Press Kit</Link>
+                        </div>
+
+                        <div className={styles.linkcolumn}>
+
+                            <p className={styles.linklabel}>Company</p>
+
+                            <Link draggable="false" href="/" className={styles.linkitem}>About us</Link>
+                            <Link draggable="false" href="/" className={styles.linkitem}>Careers</Link>
+                        </div>
+
+                        <div className={styles.linkcolumn}>
+
+                            <p className={styles.linklabel}>Legals</p>
+
+                            <Link draggable="false" href="/" className={styles.linkitem}>Terms</Link>
+                            <Link draggable="false" href="/" className={styles.linkitem}>Privacy</Link>
+                            <Link draggable="false" href="/" className={styles.linkitem}>Refund</Link>
+                        </div>
+
                     </div>
 
-                    <div className={styles.linkcolumn}>
-                        <p className={styles.linklabel}>Resources</p>
+                    <div className={styles.socialscontainer}>
+                        <Icon name='oto' fill={'#fff'} size={72} />
 
-                        <Link href="/" className={styles.linkitem}>Blogs</Link>
-                        <Link href="/" className={styles.linkitem}>Press Kit</Link>
-                    </div>
+                        <div className={styles.socials}>
+                            <Link target="_blank" draggable="false" href="https://www.linkedin.com/company/ontheorbit" className={styles.socialmedialink}>
+                                <Icon name='linkedin' fill={'#fff'} size={34} />
+                            </Link>
 
-                    <div className={styles.linkcolumn}>
-                        
-                        <p className={styles.linklabel}>Company</p>
+                            <Link target="_blank" draggable="false" href="https://www.instagram.com/ontheorbitdotcom/" className={styles.socialmedialink}>
+                                <Icon name='instagram' fill={'#fff'} size={34} />
+                            </Link>
 
-                        <Link href="/" className={styles.linkitem}>About us</Link>
-                        <Link href="/" className={styles.linkitem}>Careers</Link>
-                    </div>
-
-                    <div className={styles.linkcolumn}>
-                        
-                        <p className={styles.linklabel}>Legals</p>
-
-                        <Link href="/" className={styles.linkitem}>Terms</Link>
-                        <Link href="/" className={styles.linkitem}>Privacy</Link>
-                        <Link href="/" className={styles.linkitem}>Refund</Link>
+                            <Link target="_blank" draggable="false" href="https://www.youtube.com/@ontheorbitdotcom" className={styles.socialmedialink}>
+                                <Icon name='x' fill={'#fff'} size={34} />
+                            </Link>
+                        </div>
                     </div>
 
                 </div>
-
-                <div className={styles.socialscontainer}>
-                    <Icon name='oto' fill={'#fff'} size={68} />
-
-                    <div className={styles.socials}>
-                        <Link href="/" className={styles.socialmedialink}>
-                            <Icon name='linkedin' fill={'#fff'} size={38} />
-                        </Link>
-
-                        <Link href="/" className={styles.socialmedialink}>
-                            <Icon name='instagram' fill={'#fff'} size={38} />
-                        </Link>
-
-                        <Link href="/" className={styles.socialmedialink}>
-                            <Icon name='x' fill={'#fff'} size={38} />
-                        </Link>
-                    </div>
+                <div className={styles.footercontent}>
+                    <p style={{ textWrap: 'nowrap' }}>&copy; 2025 On The Orbit.</p>
+                    <p>
+                        All rights reserved. No part of this website or its content may be reproduced, distributed, or transmitted in any form or by any means, including photocopying, recording, or other electronic or mechanical methods, without the prior written permission of On The Orbit, except in the case of brief quotations embodied in critical reviews and certain other noncommercial uses permitted by copyright law. For permission requests, contact On The Orbit.
+                    </p>
                 </div>
-
 
             </div>
         </footer>
